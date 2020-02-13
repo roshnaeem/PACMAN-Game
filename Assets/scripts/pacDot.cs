329ea4b0d7b0d58942b6134994d17365d895f4a5
Rosheen Naeem <rosheen@xgrid.co>
2020-02-13 20:23:58 +0500

    PACMAN game built in Unity

---
+++ Assets/scripts/pacDot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pacDot : MonoBehaviour
+{
+    public static int score;
+    public Text gScore;
+    void Start()
+    {
+        score = 0;
+        gScore = GameObject.Find("Text").GetComponent<Text>();
+    }
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "pacman")
+            score++;
+        gScore.text = score.ToString();
+        Destroy(gameObject);
+    }
+}
